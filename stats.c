bec028b207b66eb6279fe0a9a2ce395d103653c3
DylShero <165214379+DylShero@users.noreply.github.com>
2025-12-01 10:19:58 +0000

    Implemented comp_doubles_asc. Added prototype for histogram.

--- stats.c
+++ stats.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 
 int comp_doubles_asc(void const * const x, void const * const y){
+    if(x > y) return 1;
+    else if (x < y) return (-1);
+    else return 0;
+}
+
+void histogram(double const * const in, int const numpoints, double * const bincentres, int * const bincounts, int const numbins){
     
 }
--- stats.h
+++ stats.h
@@ -2,5 +2,6 @@
 #define STATS_H
 
 int comp_doubles_asc(void const * const x, void const * const y);
+void histogram(double const * const in, int const numpoints, double * const bincentres, int * const bincounts, int const numbins);
 
 #endif
